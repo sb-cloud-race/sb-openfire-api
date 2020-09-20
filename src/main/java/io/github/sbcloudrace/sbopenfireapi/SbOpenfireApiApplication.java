97da87ea3f86e23dd170ec3c47169c84707acab0
Nilzao <288571+nilzao@users.noreply.github.com>
2020-09-20 08:01:33 -0400

    eureka configs

--- src/main/java/io/github/sbcloudrace/sbopenfireapi/SbOpenfireApiApplication.java
+++ src/main/java/io/github/sbcloudrace/sbopenfireapi/SbOpenfireApiApplication.java
@@ -6,11 +6,13 @@ import org.igniterealtime.restclient.entity.UserEntity;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableEurekaClient
 public class SbOpenfireApiApplication {
 
     public static void main(String[] args) {
