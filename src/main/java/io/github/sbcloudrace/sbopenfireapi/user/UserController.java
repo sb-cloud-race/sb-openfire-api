881b2f126f5c7d45b5d1094fd993e0c72a8dbc4a
Nilzao <288571+nilzao@users.noreply.github.com>
2020-09-19 23:08:51 -0400

    moving microservices

--- src/main/java/io/github/sbcloudrace/sbopenfireapi/user/UserController.java
+++ src/main/java/io/github/sbcloudrace/sbopenfireapi/user/UserController.java
@@ -1,15 +1,10 @@
 package io.github.sbcloudrace.sbopenfireapi.user;
 
-import io.github.sbcloudrace.sbopenfireapi.persona.PersonaServiceProxy;
 import lombok.AllArgsConstructor;
 import org.igniterealtime.restclient.entity.UserEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -19,24 +14,22 @@ public class UserController {
 
     private final UserServiceProxy userServiceProxy;
 
-    private final PersonaServiceProxy personaServiceProxy;
-
-    @RequestMapping(value = "/{userId}/{securityToken}", method = RequestMethod.PUT)
+    @RequestMapping(value = "/{password}", method = RequestMethod.PUT)
     @ResponseBody
-    public void createAllPersonasXmpp(@PathVariable Long userId, @PathVariable String securityToken) {
-        List<Long> personaIds = personaServiceProxy.personasIdsByUser(userId);
+    public void createAllPersonasXmpp(@RequestBody List<Long> personaIds,
+                                      @PathVariable String password) {
         personaIds.forEach(aLong -> {
             String xmppUserName = "sbrw.".concat(aLong.toString());
             try {
                 UserEntity userEntityTmp = userServiceProxy.get(xmppUserName);
-                userEntityTmp.setPassword(securityToken.substring(0, 16));
+                userEntityTmp.setPassword(password);
                 userServiceProxy.update(userEntityTmp, xmppUserName);
             } catch (Exception e) {
                 UserEntity userEntity = new UserEntity(
                         xmppUserName,
                         null,
                         null,
-                        securityToken.substring(0, 16));
+                        password);
                 userServiceProxy.create(userEntity);
             }
         });
